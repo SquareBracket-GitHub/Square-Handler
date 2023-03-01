faf0fd0df3cc1714db417a9e9c8039953abac1b1
pixel-punk-github <eomtaegyun08@gmail.com>
2023-03-01 15:48:18 +0900

    deleted

--- src/buttons/ping.js
+++
@@ -1,8 +0,0 @@
-export default {
-    id: 'ping', /* Unique ID for this button */
-    hasExtraData: false,
-    /* Execute the button's action when it is clicked by a user */
-    async execute(interaction, client) {
-        await interaction.reply('**Pong!**');
-    }
-}
